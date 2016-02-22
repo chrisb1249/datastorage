43ed7d618f47167b0bf87fdcabde72852fcc3b60
Chris Bennett <chrisb@berkeley.edu>
2016-02-22 04:25:15 +0000

    added hospitals.sql

---
+++ Exercise_1/transforming/hospitals.sql
@@ -0,0 +1,8 @@
+DROP INDEX IDX_Hosp ON Hospitals;
+DROP TABLE Hospitals;
+
+CREATE TABLE Hospitals
+ AS SELECT * FROM tbhospitals;
+
+CREATE INDEX IDX_Hosp
+ ON TABLE Hospitals (provider) AS 'BITMAP' WITH DEFERRED REBUILD;
